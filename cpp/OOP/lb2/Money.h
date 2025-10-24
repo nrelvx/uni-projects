f882b8652b7c37eb380fed4619f2c8061daf2693
nrelvx <sveronikasveronika01@gmail.com>
2025-10-24 12:11:58 +0300

    Create Money.h

---
+++ cpp/OOP/lb2/Money.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+class Money
+{
+    long hrivna;
+    unsigned char coins;
+public:
+    bool Init(long h, unsigned char c);
+    void Read();
+    void Display() const;
+    std::string toString() const;
+
+    Money Add(const Money& other) const;
+    Money Subtract(const Money& other) const;
+    Money Multiply(double factor) const;
+    Money Divide(double factor) const;
+    double Divide(const Money& other) const;
+
+    bool IsEqual(const Money& other) const;
+    bool IsGreater(const Money& other) const;
+    bool IsLess(const Money& other) const;
+};
